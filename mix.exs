4fb0ca4c410c0a1914e4ae9ed6f3e44ff58cb302
Mortada AlKhars <mortada@dbadbadba.com>
2022-09-15 22:11:26 +0300

    compile c first

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TalibEx.MixProject do
       app: :talib_ex,
       version: "0.1.0",
       elixir: "~> 1.13",
-      compilers: Mix.compilers() ++ [:talib],
+      compilers: [:talib | Mix.compilers()],
       make_makefile: "Makefile",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
